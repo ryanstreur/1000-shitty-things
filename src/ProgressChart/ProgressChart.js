7d2761c73705f59fbaa788523b3037bdc98999b9
Ryan Streur <ryan.j.streur@gmail.com>
2018-01-05 18:09:01 -0800

    fix chart

--- src/ProgressChart/ProgressChart.js
+++ src/ProgressChart/ProgressChart.js
@@ -8,9 +8,9 @@ class ProgressChart extends Component {
       data: {
           labels: ["January", "February", "March", "April", "May", "June", "July"],
           datasets: [{
-              label: "My First dataset",
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
+              label: "garbage",
+              backgroundColor: 'brown',
+              borderColor: 'orange',
               data: [0, 10, 5, 2, 20, 30, 45],
           }],
       options: {}
