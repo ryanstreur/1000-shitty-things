f3473e3e7a51158e1dd48c3a7bb03d25f45efe0f
Noah Kittleson <noahkittleson@gmail.com>
2018-01-13 00:10:04 -0800

    put up a nbunch of shitty things from Drink & Draw along with a poem I wrote

--- src/App.js
+++ src/App.js
@@ -27,7 +27,26 @@ class App extends Component {
           <li><span className="REDACTED">A link to government secrets</span> - Noah</li>
           <li><ProgressChart></ProgressChart> - Ryan</li>
           <li>A Donkey destined for greater things - Noah <p><img src='ProgressDonkey.gif' alt="donkeydonkey"/></p></li>
-          <li>My attempts at playing around with LEGO software.  I have reconstructed Greedo to be able to shoot first - AND LAST - Noah <p><img src={LegoGreedo}/></p></li>
+          <li>My attempts at playing around with LEGO software.  I have reconstructed Greedo to be able to shoot first - AND LAST - Noah <p><img src='Mecha-Greedo.png' alt="Lego Mecha-Greedo"/></p></li>
+          <li>Doodles - Noah <p><img src='drink&draw1.JPG' className="photo" alt="doodles"/></p></li>
+          <li>New Ghostbusters? - Noah <p><img src='drink&draw2.JPG' className="photo" alt="doodles"/></p></li>
+          <li>Ghost-cop and Lamp-cop - Noah & guest contributor Frisbee <p><img src='drink&draw3.JPG' className="photo" alt="ghostbuster vikins"/></p></li>
+          <li>Knight-cop and Fish-cop - Noah & guest contributor Katie <p><img src='drink&draw4.JPG' className="photo" alt="a knight and fish buddy cops"/></p></li>
+          <li>-A Poem by Noah-
+          <p>1000 things made only to be made</p>
+          <p>1000 things that fail the passing grade</p>
+          <p>3 things a day: our debt we pay to you,</p>
+          <p>our humble viewer, yearning for the new.</p>
+          <p>For when quality is in decline,</p>
+          <p>quantity will do just fine.</p>
+          <p>But this is not the time to brag</p>
+          <p>we’ve not yet filled our shitty bag!</p>
+          <p>There’s room enough for many more,</p>
+          <p>At least 984.</p>
+          <p>So back we go, to toil away</p>
+          <p>and make some shit for another day.</p></li>
+          <li>Toilet Man and a Tall Drink of Water on a date - Noah & guest contributor Amy <p><img src='drink&draw5.JPG' className="photo" alt="A dapper toilet and a tall glass of water on a date"/></p></li>
+          <li>Rose & Squid on date - Noah & guest contributor Helene<p><img src='drink&draw6.JPG' className="photo" alt="this will not end well"/></p></li>
         </ol>
       </div>
     );
